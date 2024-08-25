a296e7604146e3f6e1a892b338a6759e95bc04c4
JenningsDevelopment <jjoshua182005@gmail.com>
2024-08-25 16:57:15 -0400

    Add files via upload

---
+++ jd-hud/client.lua
@@ -0,0 +1,13 @@
+Citizen.CreateThread(function()
+    ReplaceHudColourWithRgba(
+      116, -- old Color
+      132, -- R
+      102, -- G
+      226, -- B
+      255 -- A
+    )
+  end)  --I have put a list of colours in hudcolors.txt thx to @AvaN0x.
+
+  AddTextEntry('FE_THDR_GTAO', 'Your Server Name')  --Just Change where it says 'your server Name'.
+
+ 
---
+++ jd-hud/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+
+games { 'gta5' }
+
+author 'Jennings Dev'
+description 'Simple Script that changes hud  colour'
+version '1.0.0'
+
+
+
+client_script("client.lua")
